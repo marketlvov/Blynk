1363f5d7d800659be9161471a257ddfb08b24afe
Iurii Ivanovych <iurii.ivanovych@globallogic.com>
2022-04-18 18:51:00 +0300

    Blynk- pulmode (J-Link no stoped)

--- src/board_STM32F407VET6.c
+++ src/board_STM32F407VET6.c
@@ -2,28 +2,31 @@
 // board_STM32F407VET6
 
 #include "stm32f4xx.h"
-#define VET_GPIOD_P15_ON       0xF000    // on  GPIOD  pin15
-#define VET_GPIOD_P15_OFF      0x0000    // off GPIOD  pin15
-#define VET_GPIOD_P15_UOTPUT   0x4000000 // GPIOD pin 15 OUTPUT
-#define VET_GPIOD_ALL_PULLUP   0x0       // pullup all port GPIOD
-#define VET_GPIOD_ALL_LOWSPEED 0x0       // GPIOD low speed to all port
+#define VET_GPIOA_P6_ON       0x0040     // on  GPIOA  p6on p7off
+#define VET_GPIOA_P6_OFF      0x0080     // off GPIOA  p7on p6off
+#define VET_GPIOA_P6_UOTPUT   0x00005000 // GPIOA pin 6,7 OUTPUT
+#define VET_GPIOA_ALL_PULLUP   0x0        // pullup all port GPIOA
+#define VET_GPIOA_ALL_LOWSPEED 0x0        // GPIOA low speed to all port
+
+
 
 /* function implementation */ 
 
 // board_STM32F407VET6 initialization
 void board_init(void){
-    RCC->AHB1ENR  |= RCC_AHB1ENR_GPIODEN;    // on  GPIOD  pin15
-    GPIOD->MODER   = VET_GPIOD_P15_UOTPUT;   // GPIOD pin 15 OUTPUT
-    GPIOD->OTYPER  = VET_GPIOD_ALL_PULLUP;   // pullup all port GPIOD
-    GPIOD->OSPEEDR = VET_GPIOD_ALL_LOWSPEED; // GPIOD low speed to all port
+    RCC->AHB1ENR  |= RCC_AHB1ENR_GPIOAEN;    // on  GPIOA  rcc
+    GPIOA->MODER   = VET_GPIOA_P6_UOTPUT;    // GPIOA PA-6,7 to OUTPUT
+    GPIOA->OTYPER  = VET_GPIOA_ALL_PULLUP;   // pullup all port GPIOA
+    GPIOA->OSPEEDR = VET_GPIOA_ALL_LOWSPEED; // GPIOA low speed to all port
 }
+
 
 void qp_blink_turn_off(void) {
     // ("LED OFF\n");
-    GPIOD->ODR = VET_GPIOD_P15_OFF; // off GPIOD  pin15 
+    GPIOA->ODR = VET_GPIOA_P6_OFF; // off GPIOD  pin15 
 }
 
 void qp_blink_turn_on(void) {
     // ("LED ON\n");
-    GPIOD->ODR = VET_GPIOD_P15_ON; // on GPIOD  pin15 
+    GPIOA->ODR = VET_GPIOA_P6_ON; // on GPIOD  pin15 
 }
